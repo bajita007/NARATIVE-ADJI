66c6578a2c0d64b642979275b3609e2f48663996
Muhammad Dzul Arsyil <dzularsyil.muhammad@gmail.com>
2023-08-10 00:19:13 +0700

    chore: Initialize xcode project

---
+++ NarrativePhone/NarrativePhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarrativePhoneApp.swift
+//  NarrativePhone
+//
+//  Created by Muhammad Dzul Arsyil on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NarrativePhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
