624cf476fdcbbb505906dcde1178f61ea6b65ae0
Muhammad Dzul Arsyil <dzularsyil.muhammad@gmail.com>
2023-08-12 09:36:52 +0700

    chore: remove unused files

--- NarrativePhone/Shared/Models/Contact.swift
+++
@@ -1,8 +0,0 @@
-// TODO: remove this file and use AddressEntry struct
-struct Contact: Identifiable {
-    var id :Int
-    var contact: String
-    var group: String
-    var desk: String
-    var fav: Bool
-}
